7fad8f67cac8eaf93fc6289023bbfe836039bfa1
DavidBlanchGit <david.blanch.dev@gmail.com>
2025-02-02 16:54:59 +0100

    02/02/2025

--- port_scanner/port_scan.cpp
+++ port_scanner/port_scan.cpp
@@ -9,11 +9,18 @@ void scannig(int amountPorts, std::string IP);
 
 int main(){
 
-    int lastPort {100};
+    int lastPort {0};
     std::string IP {localhost};
 
-    std::cout << "How many ports you want to scan (1-X): ";
-    std::cin >> lastPort;
+    do{
+        std::cout << "How many ports you want to scan (1-X): ";
+        std::cin >> lastPort;
+
+        if (lastPort < 1){
+            std::cout << "Port have to be bigger than 1" << std::endl;
+        }
+        
+    } while (lastPort < 1);
 
     scannig(lastPort, IP);
 
@@ -40,16 +47,20 @@ bool portIsOpen(const std::string& address, int port){
 void scannig(int amountPorts, std::string IP){
 
     bool allPortsClosed {true};
+    int openPorts {0};
 
     std::cout << "Scanning ports from 1 - " << amountPorts << std::endl;
 
     for (int i = 1; i < amountPorts+1; i++){
         if (portIsOpen(IP, 80))
             std::cout <<  "Port "<< i << " : OPEN" << std::endl;
+            openPorts++;
             allPortsClosed = false;
     }
 
     if (!allPortsClosed){
         std::cout << "No ports are OPEN" << std::endl;
+    }else{
+        std::cout << "Ports OPEN: " << openPorts << std::endl;
     }
 }
