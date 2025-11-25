9636b4ddc07e3c22d6bd6c5ee90d1fb79c9db9f5
David Blanch <david.blanch.dev@gmail.com>
2025-11-25 14:19:38 +0100

    25/11/2025

---
+++ easy/To_Do_List.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+class Task {
+public:
+  std::string name;
+  std::string description;
+  Task(std::string name, std::string description) {
+    this->name = name;
+    this->description = description;
+  }
+
+private:
+};
+
+int main(int argc, char *argv[]) {
+
+  std::vector<Task> tasks;
+
+  while (true) {
+
+    break;
+  }
+
+  return 0;
+}
