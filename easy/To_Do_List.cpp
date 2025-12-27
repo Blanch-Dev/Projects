5a2f6ffe1861d719058594fd544ad3c05cb1871c
David Blanch <david.blanch.dev@gmail.com>
2025-12-27 06:17:42 +0100

    27/12/2025

--- easy/To_Do_List.cpp
+++ easy/To_Do_List.cpp
@@ -1,3 +1,4 @@
+#include <exception>
 #include <iostream>
 #include <ostream>
 #include <string>
@@ -7,9 +8,11 @@ class Task {
 public:
   std::string name;
   std::string description;
+  bool finished;
   Task(std::string name, std::string description) {
     this->name = name;
     this->description = description;
+    finished = false;
   }
 
 private:
@@ -31,49 +34,69 @@ int menu() {
   int option;
   std::cout << "OPTION: ";
   std::cin >> option;
+  std::cin.ignore();
   std::cout << std::endl;
   return option;
 }
 
-int addTask() {
-  std::string name, description;
-  std::cout << "Name: ";
-  // ERROR
-  std::getline(std::cin, name);
-  std::cout << "Description: ";
-  std::getline(std::cin, description);
-  tasks.insert(tasks.end(), Task(name, description));
+int addTask(std::string *name, std::string *description) {
+  tasks.insert(tasks.end(), Task(*name, *description));
   return 0;
+}
+
+int deleteTask(int taskId) {
+  try {
+    tasks.at(taskId - 1).finished = true;
+    return 0;
+  } catch (const std::exception) {
+    std::cout << "Task ID is not valid.";
+    return 1;
+  }
 }
 
 void showTasks() {
   int numOfTask = 1;
   for (Task it : tasks) {
-    std::cout << "[ ]" << std::endl
-              << numOfTask << std::endl
+    std::cout << "[ ]  " << numOfTask << std::endl
               << "Name: " << it.name << std::endl
               << "Description: " << it.description << std::endl;
   }
 }
 
-int main(int argc, char *argv[]) {
-
+void mainLoop() {
   bool keepGoing = true;
 
   while (keepGoing) {
     switch (menu()) {
-    case 1:
-      addTask();
+    case 1: {
+      std::string name, description;
+      std::cout << "Name: ";
+      std::getline(std::cin, name);
+      std::cout << "Description: ";
+      std::getline(std::cin, description);
+      addTask(&name, &description);
       break;
-    case 2:
+    }
+    case 2: {
+      int taskId;
+      std::cout << "What is the ID of the task you want to end: ";
+      std::cin >> taskId;
+      deleteTask(taskId);
       break;
-    case 3:
+    }
+    case 3: {
       showTasks();
       break;
+    }
     default:
       keepGoing = false;
     }
   }
+}
+
+int main(int argc, char *argv[]) {
+
+  mainLoop();
 
   return 0;
 }
